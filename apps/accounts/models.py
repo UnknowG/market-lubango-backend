3728562da74baefc4bd3a88d99d7b9385daf2c06
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-10-08 22:36:52 +0100

    feat(accounts-model): Create the Custom User Model.

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    TYPE_USER = [
+        ("buyer", "Comprador"),
+        ("seller", "Vendedor"),
+        ("admin", "Administrador"),
+    ]
+
+    email = models.EmailField(unique=True)
+    user_type = models.CharField(max_length=10, choices=TYPE_USER, default="buyer")
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birth_date = models.DateField(null=True, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    avatar_url = models.URLField(blank=True, null=True)
+    is_approved_seller = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.username = self.email
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.email
