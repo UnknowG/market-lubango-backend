7e38ae4dd4a580dd6067850823b6990e060520e9
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-10-11 20:25:41 +0100

    feat(models): Create the Product model

--- apps/products/models.py
+++ apps/products/models.py
@@ -19,3 +19,42 @@ class Category(models.Model):
                 counter += 1
             self.slug = unique_slug
         super().save(*args, **kwargs)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    slug = models.SlugField(unique=True, blank=True)
+    image = models.ImageField(upload_to="product_img", blank=True, null=True)
+    featured = models.BooleanField(default=False)
+    in_stock = models.BooleanField(default=True)
+    stock_quantity = models.PositiveIntegerField(default=1)
+    category = models.ForeignKey(
+        Category,
+        related_name="products",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    store = models.ForeignKey(
+        "accounts.Store",
+        on_delete=models.CASCADE,
+        related_name="products",
+    )
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+            unique_slug = self.slug
+            counter = 1
+            while Product.objects.filter(slug=unique_slug).exists():
+                unique_slug = f"{self.slug}-{counter}"
+                counter += 1
+            self.slug = unique_slug
+        super().save(*args, **kwargs)
