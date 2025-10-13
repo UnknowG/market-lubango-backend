2d1ad4bef1ca1a295bf0eaece5c8c6da38a19b7d
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-10-13 23:15:55 +0100

    feat(serializer): Create the serializer for category

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Category, Product
 
 
 class ProductListSerializer(serializers.ModelSerializer):
@@ -17,3 +17,17 @@ class ProductDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ["id", "name", "slug", "description", "image", "price", "store", "category", "in_stock", "stock_quantity", "created_at"]
+
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name", "image", "slug"]
+
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
+    products = ProductListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = ["id", "name", "image", "products"]
