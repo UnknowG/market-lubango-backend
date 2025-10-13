ba32e155eab2987d2273e0566f7e17b83eb0f73d
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-10-13 23:40:39 +0100

    feat(views): Create the api views for show the category detail

--- apps/products/views.py
+++ apps/products/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from .models import Category, Product
-from .serializers import CategoryListSerializer, ProductDetailSerializer, ProductListSerializer
+from .serializers import CategoryDetailSerializer, CategoryListSerializer, ProductDetailSerializer, ProductListSerializer
 
 
 @api_view(["GET"])
@@ -40,3 +40,17 @@ def category_list(request):
     categories = Category.objects.all()
     serializer = CategoryListSerializer(categories, many=True)
     return Response(serializer.data)
+
+
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def category_detail(request, slug):
+    try:
+        category = Category.objects.get(slug=slug)
+        serializer = CategoryDetailSerializer(category)
+        return Response(serializer.data)
+    except Category.DoesNotExist:
+        return Response(
+            {"error": "Category not found"},
+            status=status.HTTP_404_NOT_FOUND
+        )
