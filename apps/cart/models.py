72674302e9c11eea4ea693935c54cb5928654711
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-10-16 15:50:49 +0100

    feat(models): Create the cart model

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Cart(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_MODEL_USER,
+        on_delete=models.CASCADE,
+        related_name="carts",
+        blank=True,
+        null=True,
+    )
+    cart_code = models.CharField(max_length=11, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.cart_code
