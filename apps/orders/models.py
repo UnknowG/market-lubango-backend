6e2ed378ad5baafbf57a95e6317974d369b63137
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-11-02 00:10:57 +0100

    feat(models): Create the models for item order

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -41,3 +41,13 @@ class Order(models.Model):
     
     def __str__(self):
         return self.order_number
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey("products.Product", on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} pedido: {self.order.order_number}."
