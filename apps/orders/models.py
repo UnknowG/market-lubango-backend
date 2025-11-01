4870248f65aca8b5537958b55bf90f8fd4f9bb17
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-11-01 05:10:51 +0100

    feat(order): Create the model for Order.

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.conf import settings
+import uuid
 
-# Create your models here.
+
+class Order(models.Model):
+    ORDER_STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('confirmed', 'Confirmed'),
+        ('processing', 'Processing'),
+        ('shipped', 'Shipped'),
+        ('delivered', 'Delivered'),
+        ('cancelled', 'Cancelled'),
+    ]
+    
+    PAYMENT_STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('paid', 'Paid'),
+        ('failed', 'Failed'),
+        ('refunded', 'Refunded'),
+    ]
+
+    order_number = models.CharField(max_length=20, unique=True, editable=False)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="orders"
+    )
+    status = models.CharField(max_length=20, choices=ORDER_STATUS_CHOICES, default="pending")
+    payment_status = models.CharField(max_length=20, choices=PAYMENT_STATUS_CHOICES, default="pending")
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    shipping_adderss = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.order_number:
+            # Gerar um único número para cada pedido
+            self.order_number = f"ORD-{uuid.uuid4().hex[:8].upper()}"
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.order_number
