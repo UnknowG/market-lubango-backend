8c90772eb7c8bed6dfb939503c176a78370a11fd
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-11-03 15:19:11 +0100

    feat(models): Create the model for rating the product

--- apps/reviews/models.py
+++ apps/reviews/models.py
@@ -27,3 +27,12 @@ class Review(models.Model):
     class Meta:
         unique_together = ["product", "user"]
         ordering = ["-created_at"]
+
+
+class ProductRating(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name="rating")
+    average_rating = models.FloatField(default=0.0)
+    total_reviews = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.average_rating} ({self.total_reviews}) reviews."
