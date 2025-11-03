1b28faff6cc4f730cc1dcd5c17d35fe46293ff0d
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-11-03 15:15:09 +0100

    feat(models): Create the model for Reviews

--- apps/reviews/models.py
+++ apps/reviews/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
+from apps.products.models import Product
 
-# Create your models here.
+
+class Review(models.Model):
+    RATINGS_CHOICES = [
+        (1, "Poor"),
+        (2, "Fair"),
+        (3, "Good"),
+        (4, "Very Good"),
+        (5, "Excellent"),
+    ]
+
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="reviews"
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="reviews")
+    rating = models.PositiveIntegerField(choices=RATINGS_CHOICES)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s review of {self.product.name}"
+    
+    class Meta:
+        unique_together = ["product", "user"]
+        ordering = ["-created_at"]
