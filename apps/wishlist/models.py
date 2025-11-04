19147769b3030d890d01b118e95ae424e6b7637f
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-11-04 14:19:39 +0100

    feat(models): Create the models for Wishlist.

--- apps/wishlist/models.py
+++ apps/wishlist/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+from apps.products.models import Product
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="wishlist"
+    )
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="wishlist_items"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ["user", "product"]
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product.name} in wishlist"
