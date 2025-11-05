f30e7ed9d9b0a10e1c9666e2fb2f532e76f8d91e
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-11-05 22:06:09 +0100

    fix(urls-cart): Reduce the redoding call in endpoints in orders app.

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -3,16 +3,16 @@ from . import views
 
 urlpatterns = [
     # Order management
-    path("orders/create/", views.create_order, name="create_order"),
-    path("orders/", views.get_user_orders, name="user_orders"),
-    path("orders/<str:order_number>/", views.get_order_detail, name="order_detail"),
+    path("create/", views.create_order, name="create_order"),
+    path("", views.get_user_orders, name="user_orders"),
+    path("<str:order_number>/", views.get_order_detail, name="order_detail"),
     path(
-        "orders/<str:order_number>/refund/", views.request_refund, name="request_refund"
+        "<str:order_number>/refund/", views.request_refund, name="request_refund"
     ),
     # Seller order management
-    path("seller/orders/", views.get_store_orders, name="store_orders"),
+    path("seller/", views.get_store_orders, name="store_orders"),
     path(
-        "seller/orders/<str:order_number>/status/",
+        "seller/<str:order_number>/status/",
         views.update_order_status,
         name="update_order_status",
     ),
