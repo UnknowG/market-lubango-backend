b4df58585065ef97140939799da4e5c86a464815
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-11-07 15:41:44 +0100

    test(category): Create the test for create the new category.

--- apps/products/tests.py
+++ apps/products/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Category
 
-# Create your tests here.
+User = get_user_model()
+
+
+class CategoryModelTest(TestCase):
+    """Testes para o modelo Category"""
+
+    def test_create_category(self):
+        """Testa a criação de uma categoria"""
+
+        category = Category.objects.create(
+            name="Test Category",
+        )
+        self.assertEqual(category.name, "Test Category")
+        self.assertTrue(category.slug) # Verifica se o slug foi gerado automaticamente
