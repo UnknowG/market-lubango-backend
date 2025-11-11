abd67ee9e4a9548eeb613d8f42a160b8a08a490b
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-11-11 12:34:21 +0100

    test(wishlist): Test the wishlist model: > - Wishlist creation; > - Wishlist str; > - Unique together between user and product.

--- apps/wishlist/tests.py
+++ apps/wishlist/tests.py
@@ -27,3 +27,21 @@ class WishlistModelTest(TestCase):
             user=self.user,
             product=self.product,
         )
+
+    def test_wishlist_creation(self):
+        """Testa a criação de um item na lista de desejos"""
+        self.assertEqual(self.wishlist_item.user, self.user)
+        self.assertEqual(self.wishlist_item.product, self.product)
+
+    def test_wishlist_str(self):
+        """Testa o método __str__ do modelo Wishlist"""
+        expected_str = f"{self.user.username} - {self.product.name} na lista de desejos"
+        self.assertEqual(str(self.wishlist_item), expected_str)
+
+    def test_unique_together(self):
+        """Testa a restrição de unicidade entre usuário e produto"""
+        with self.assertRaises(Exception):
+            Wishlist.objects.create(
+                user=self.user,
+                product=self.product,
+            )
