f5a9b7431c4a794acafe7a67375bf42b25210121
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-11-11 13:06:54 +0100

    fix(signal): Add the signal on main review app config.

--- apps/reviews/apps.py
+++ apps/reviews/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class ReviewsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.reviews"
+
+    def ready(self):
+        """
+        Importar o signal de criação de reviews para atualizar o rating do produto.
+        """
+        import apps.reviews.signals
